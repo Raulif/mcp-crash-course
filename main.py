de82b5861c9e7e880e553b7b84d04b49dfc08a2f
Raulif <raul4cade@gmail.com>
2025-07-11 08:47:10 +0200

    Enhance main.py and update dependencies for Google AI integration
    - Added Google Generative AI model initialization in main.py.
    - Configured StdioServerParameters for math server execution.
    - Updated pyproject.toml to include langchain-google-genai and isort dependencies.
    - Modified uv.lock to reflect new package resolutions and added cachetools.
    - Minor adjustments to weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
-print(os.getenv("GEMINI_API_KEY")) 
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-001"
+)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/home/rfourcade/dev/mcp-course/mcp-crash-course/servers/math_server.py"],
+)
+
 
 async def main():
     print("Hello from mcp-crash-course!")
