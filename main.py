e2d28cbfeba1265de4114aebc46354e7b26aacee
Raulif <raul4cade@gmail.com>
2025-07-11 09:58:55 +0200

    Implement async session handling and enhance math server logging
    - Integrated async session management in main.py for improved interaction with the math server.
    - Added logging for input parameters in the add and multiply functions of math_server.py to aid in debugging.
    - Utilized HumanMessage for structured input in the agent's invocation.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from httpx import Client
+from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
@@ -20,9 +22,18 @@ stdio_server_params = StdioServerParameters(
 )
 
 
+
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize();
+            print("session initialize")
+            tools = await load_mcp_tools(session)
 
+            agent = create_react_agent(llm,tools)
 
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 3 * 50 + 10?")]})
+            print(result['messages'][-1].content)
+            
 if __name__ == "__main__":
     asyncio.run(main())
--- servers/math_server.py
+++ servers/math_server.py
@@ -6,11 +6,13 @@ mcp = FastMCP("Math")
 @mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
+    print("<ADD> ","a: ", a, "b: ", b)
     return a + b
 
 @mcp.tool()
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
+    print("<MULTIPLY> ","a: ", a, "b: ", b)
     return a * b
 
 if __name__ == "__main__":
